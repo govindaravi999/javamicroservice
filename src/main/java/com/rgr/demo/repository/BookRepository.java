fc7d33ec66b17d1cb4f2a89ac23ff80dae37756e
ckg6 <gravipati@moneygram.com>
2020-03-26 21:48:10 -0500

    initial service

---
+++ src/main/java/com/rgr/demo/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.rgr.demo.repository;
+
+import com.rgr.demo.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book,Long> {
+}
